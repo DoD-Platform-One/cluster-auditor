678860e9753a3ffd6ac0fcff36e288e38ef64c1b
Ronnie Webb <rwebb@revacomm.com>
2021-11-15 15:50:25 -0600

    changing grafana url

--- chart/tests/cypress/grafana.spec.js
+++ chart/tests/cypress/grafana.spec.js
@@ -1,6 +1,6 @@
 describe('Grafana Dashboard', function() {
   it('Check Endoing is accessible', function() {
-      cy.visit('http://monitoring-monitoring-grafana/d/YBgRZG6Mz/opa-violations?orgId=1', {
+      cy.visit('http://monitoring-grafana/d/YBgRZG6Mz/opa-violations?orgId=1', {
           auth: {
               username: 'admin',
               password: 'prom-operator',
