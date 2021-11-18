dd74a1bc9f9b07ebe5070be9667f0fb4f65f3cb2
Ronnie Webb <rwebb@revacomm.com>
2021-11-18 15:45:01 -0600

    changing label

--- chart/tests/cypress/grafana.spec.js
+++ chart/tests/cypress/grafana.spec.js
@@ -1,6 +1,6 @@
 describe('Grafana Dashboard', function() {
   it('Check Endoing is accessible', function() {
-      cy.visit('http://monitoring-monitoring-grafana.monitoring.svc.cluster.local/d/YBgRZG6Mz/opa-violations?orgId=1', {
+      cy.visit('http://monitoring-grafana.monitoring.svc.cluster.local/d/YBgRZG6Mz/opa-violations?orgId=1', {
           auth: {
               username: 'admin',
               password: 'prom-operator',
