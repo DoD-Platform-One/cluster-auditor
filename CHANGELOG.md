22c0284fcbcb716cf2a5a9dc57c29921ac4a37b8
Jimmy Bourque <jbourque@bridgephase.com>
2023-08-29 14:07:29 +0000

    Updated test for Prometheus to ensure it works when there are many more targets loaded

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,10 @@
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/), and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ---
+## [1.5.0-bb.7] - 2023-08-24
+### Fixed
+- Updated test to work when there are multiple targets present
+
 ## [1.5.0-bb.6] - 2023-08-22
 ### Fixed
 - Updated namespace for grafana dependency and reverted test values back to using the monitoring namespace
--- chart/tests/cypress/01-prometheus.spec.js
+++ chart/tests/cypress/01-prometheus.spec.js
@@ -1,4 +1,3 @@
-
 describe('Prometheus Targets', {
   // Wait up to 2 minutes (4 sec x 30 attempts) for target to be shown before failing
   retries: {
@@ -7,7 +6,9 @@ describe('Prometheus Targets', {
 }, () => {
     it('Validate opa exporter metrics are scraped', function() {
       cy.visit(`${Cypress.env('prometheus_url')}/targets`)
-      cy.get(`a[href*="serviceMonitor/${Cypress.env('reporter_ns')}"]`)
+      cy.get('button:contains("cluster-auditor\/opa-exporter\/0")')
+        .click({force: true})
+      cy.get('a[href*="cluster-auditor\/opa-exporter\/0"]')
         .should('be.visible')
         .and('contain', '(1/1 up)')
     })
