5d7123dc2a4e2535739bf801dfadeea88b3b3241
Bulat Khamitov <27224-bkhamitov@users.noreply.gitlab.example.com>
2025-04-15 14:39:50 +0000

    Update Cypress tests

--- chart/tests/cypress/e2e/01-prometheus.cy.js
+++ chart/tests/cypress/e2e/01-prometheus.cy.js
@@ -1,11 +1,26 @@
+Cypress.Commands.add('validatePromTargetNew', (monitorText, match) => {
+  cy.get(`input[placeholder*="Select scrape pool"]`)
+    .click({force: true})
+  cy.get(`div[value="${monitorText}"]`)
+    .click({force: true})
+  cy.contains(monitorText)
+    .should('exist')
+  cy.contains(match)
+    .should('exist')
+})
+
 describe('Prometheus Targets', {
+
   // Wait up to 2 minutes (4 sec x 30 attempts) for target to be shown before failing
   retries: {
     runMode: 5,
   }
 }, () => {
+  
+    const variableUpMatch = /\d+\s\/\s\d+\sup/;
+    
     it('Validate opa exporter metrics are scraped', function() {
       cy.visit(`${Cypress.env('prometheus_url')}/targets`)
-      cy.validatePromTarget('cluster-auditor\/opa-exporter\/0', '(1/1 up)' )
+      cy.validatePromTargetNew('serviceMonitor\/cluster-auditor\/opa-exporter\/0', variableUpMatch)
     })
 })
